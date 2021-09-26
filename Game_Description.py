9fff141c7299b7f83962639fe8a403f2ff967afe
William Martin <you@example.com>
2021-09-26 12:04:51 -0500

    Started project

---
+++ Game_Description.py
@@ -0,0 +1,12 @@
+## Game Description:
+## In Alien Invasion, the player controls a rocketship that appears
+## at the bottom of the screen. The player can move the ship
+## right and left using the arrow keys and shoot bullets using the
+## spacebar. When the game begins, a fleet of aliens fills the sky
+## and moves across and down the screen. The player shoots and
+## destroys the aliens. If the player shoots all the aliens, a new fleet
+## appears that move faster than the previous fleet. If any alien hits
+## the player's ship or reaches the bottom of the screen, the player
+## loses the ship. If the player loses three ships, the game ends.
+
+## pg. 235
