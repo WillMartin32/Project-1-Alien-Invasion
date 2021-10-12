3a3e488054ca96ae21b0fcbd7deb864f532a6b97
William Martin <you@example.com>
2021-10-11 20:26:56 -0500

    256 to 265 - made alien ships appear

--- Game_Description.py
+++ Game_Description.py
@@ -9,4 +9,4 @@
 ## the player's ship or reaches the bottom of the screen, the player
 ## loses the ship. If the player loses three ships, the game ends.
 
-## pg. 256 Starte of Ch. 13
+## pg. 265 Making the Fleet Move
